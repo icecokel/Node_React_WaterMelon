f11debc62105bd5f753e8e91c5e034e45e6a305f
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-08-30 02:30:27 +0900

    Refactor : App Feat : Modify Login Info

--- views/src/components/Header.tsx
+++ views/src/components/Header.tsx
@@ -1,8 +1,32 @@
-const Header = () => {
+import { useEffect, useState } from "react";
+
+const defaultData = {
+  accountInfo: {
+    email: "",
+    nickName: "",
+  },
+};
+
+const Header = (props: any) => {
+  const [accountInfo, setAccountInfo] = useState<any>(defaultData.accountInfo);
+
+  useEffect(() => {
+    const localData = window.sessionStorage.getItem("accountInfo");
+    localData && setAccountInfo(JSON.parse(localData));
+  }, []);
+
+  const onClickLogout = () => {
+    window.sessionStorage.removeItem("accountInfo");
+    window.localStorage.removeItem("accountInfo");
+    props.setIsLogined(false);
+  };
   return (
     <header className="header_box">
       <div>Logo</div>
-      <div>Login Info</div>
+      <div className="account_info_box">
+        <span> {accountInfo.nickName}</span>
+        <label onClick={onClickLogout}>로그아웃</label>
+      </div>
     </header>
   );
 };
