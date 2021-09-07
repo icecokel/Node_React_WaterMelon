70526cbfc5ad171fa8d96e3855aac27f2f466190
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-09-07 09:46:16 +0900

    Feat : Add SQL

---
+++ SqlQuerys/createTable.sql
@@ -0,0 +1,16 @@
+-- 기존 테이블 날리기
+DROP TABLE USERS;
+
+-- USERS 테이블
+
+CREATE TABLE USERS (
+user_id INT(100) PRIMARY KEY AUTO_INCREMENT COMMENT '사용자 코드',
+email VARCHAR(30) NOT NULL COMMENT '사용자 이메일',
+PASSWORD VARCHAR(30) NOT NULL COMMENT '비밀번호', 
+sns VARCHAR(30) COMMENT 'SNS 로그인 여부', 
+create_date DATETIME DEFAULT NOW() COMMENT '사용자 가입일',
+update_date DATETIME COMMENT '수정일',
+nickname VARCHAR(30) NOT NULL COMMENT '별명',
+profile_image VARCHAR(50) COMMENT '로그인 여부',
+del_yn VARCHAR(1) CHECK(del_yn IN ('Y','N')) DEFAULT 'N' COMMENT '삭제 여부'
+)DEFAULT CHARSET = utf8;
---
+++ SqlQuerys/insertDummyData.sql
@@ -0,0 +1,13 @@
+-- 임시 계정 생성
+
+INSERT INTO USERS (
+	email 
+	, PASSWORD 
+	, sns
+	, nickname 
+) VALUES (
+	'asd@asd.com'
+	,'asd'
+	,'1'
+	,'관리자'
+);
