46094123831bd74dd42e514a269372c58863a10b
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-09-27 18:07:47 +0900

    Feat : Add Tables;

--- SqlQuerys/insertDummyData.sql
+++ SqlQuerys/insertDummyData.sql
@@ -23,3 +23,27 @@ INSERT INTO USERS (
 	,'1'
 	,'관리자'
 );
+
+INSERT INTO USERS (
+	email 
+	, user_passwd 
+	, sns
+	, nickname 
+) VALUES (
+	'asd1@asd.com'
+	,'asd'
+	,'1'
+	,'친구1'
+);
+
+INSERT INTO USERS (
+	email 
+	, user_passwd 
+	, sns
+	, nickname 
+) VALUES (
+	'asd2@asd.com'
+	,'asd'
+	,'1'
+	,'친구2'
+);
