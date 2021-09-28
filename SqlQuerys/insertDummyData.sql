122a13f919344a16911c2af39112f10a7d22a8f6
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-09-28 09:58:54 +0900

    Refactor : Modify Querys

--- SqlQuerys/insertDummyData.sql
+++ SqlQuerys/insertDummyData.sql
@@ -1,49 +1,13 @@
 -- 임시 계정 생성
 
-INSERT INTO USERS (
-	email 
-	, user_passwd 
-	, sns
-	, nickname 
-) VALUES (
-	'ice@coke.com'
-	,'asd'
-	,'1'
-	,'얼음콜라'
-);
+INSERT INTO USERS ( email, user_passwd, sns, nickname ) 
+VALUES ('ice@coke.com', 'asd', '1' ,'얼음콜라');
 
-INSERT INTO USERS (
-	email 
-	, user_passwd 
-	, sns
-	, nickname 
-) VALUES (
-	'asd@asd.com'
-	,'asd'
-	,'1'
-	,'관리자'
-);
+INSERT INTO USERS ( email, user_passwd, sns, nickname ) 
+VALUES ('asd@asd.com', 'asd', '1' ,'관리자');
 
-INSERT INTO USERS (
-	email 
-	, user_passwd 
-	, sns
-	, nickname 
-) VALUES (
-	'asd1@asd.com'
-	,'asd'
-	,'1'
-	,'친구1'
-);
+INSERT INTO USERS ( email, user_passwd, sns, nickname ) 
+VALUES ('frd1@coke.com', 'asd', '1' ,'친구1');
 
-INSERT INTO USERS (
-	email 
-	, user_passwd 
-	, sns
-	, nickname 
-) VALUES (
-	'asd2@asd.com'
-	,'asd'
-	,'1'
-	,'친구2'
-);
+INSERT INTO USERS ( email, user_passwd, sns, nickname ) 
+VALUES ('frd2@coke.com', 'asd', '2' ,'친구2');
