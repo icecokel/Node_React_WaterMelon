3503986b31a257ae75e226568ae03a30cf42d05b
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-10-12 19:10:38 +0900

    Feat : Add ChatTab

---
+++ views/src/components/ChatTab.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+export enum TabMode {
+  FRIENDS = 0,
+  ROOM = 1,
+}
+
+const ChatTab = (props: any) => {
+  const [mode, setMode] = useState<number>(TabMode.FRIENDS);
+
+  return (
+    <div className="chatList_box">
+      <div className="chatList_tab">
+        <label
+          style={
+            mode === TabMode.FRIENDS
+              ? { backgroundColor: "#f28585", color: "#FFFFFF" }
+              : {}
+          }
+          className="friends_lbl"
+          onClick={() => {
+            setMode(TabMode.FRIENDS);
+          }}
+        >
+          친구목록
+        </label>
+        <label
+          style={
+            mode === TabMode.ROOM
+              ? { backgroundColor: "#c2d991", color: "#FFFFFF" }
+              : {}
+          }
+          className="room_lbl"
+          onClick={() => {
+            setMode(TabMode.ROOM);
+          }}
+        >
+          대화방
+        </label>
+      </div>
+      <div className="chatList_contents">
+        {mode === TabMode.FRIENDS && (
+          <div className="chatList_friends">친구 목록</div>
+        )}
+        {mode === TabMode.ROOM && (
+          <div className="chatList_room">채팅방 목록</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ChatTab;
