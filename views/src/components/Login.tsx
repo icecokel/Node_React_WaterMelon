199e24a1da0c1b449636189ed97190901d560829
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-10-30 00:03:09 +0900

    Feat :modify Login

--- views/src/components/Login.tsx
+++ views/src/components/Login.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import watermelon from "../styles/images/watermelon01.jpg";
 import google from "../styles/images/google_icon.png";
 import kakao from "../styles/images/kakao_icon.png";
 import naver from "../styles/images/naver_icon.png";
+import userEvent from "@testing-library/user-event";
 
 const Login = (props: any) => {
   const callAPI: Function = props.callAPI;
@@ -12,6 +13,10 @@ const Login = (props: any) => {
     email: "",
     password: "",
     isRemember: false,
+  });
+
+  useEffect(() => {
+    loginCheck();
   });
 
   const onPressEnter = (e: any) => {
@@ -60,91 +65,103 @@ const Login = (props: any) => {
     } else {
       window.sessionStorage.setItem("nickname", res.nickname);
       props.setIsLogined(res.isLogined);
+      props.history.push("/");
+    }
+  };
+
+  const loginCheck = async () => {
+    const res = await callAPI({
+      url: "/user/check",
+      method: "GET",
+    });
+    window.sessionStorage.setItem("nickname", res.nickname);
+    props.setIsLogined(res.isLogined);
+
+    if (res.isLogined) {
+      props.history.push("/");
     }
   };
   return (
-    <article className="modal">
-      <div className="login_box">
-        <div className="box">
-          <div className="image_box">
-            <img src={watermelon} alt="loginImg" className="login_big_img" />
-          </div>
-          <div className="login_content">
-            <h2>로그인</h2>
-            <form>
+    <div className="login_box">
+      <div className="box">
+        <div className="image_box">
+          <img src={watermelon} alt="loginImg" className="login_big_img" />
+        </div>
+        <div className="login_content">
+          <h2>로그인</h2>
+          <form>
+            <input
+              type="email"
+              value={formData.email}
+              onKeyPress={onPressEnter}
+              onChange={(e) => {
+                setFormData({ ...formData, email: e.target.value });
+              }}
+            />
+            <input
+              type="password"
+              value={formData.password}
+              autoComplete="off"
+              onKeyPress={onPressEnter}
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  password: e.target.value,
+                });
+              }}
+            />
+          </form>
+          <div className="login_question">
+            <label>
               <input
-                type="email"
-                value={formData.email}
-                onKeyPress={onPressEnter}
+                type="checkbox"
                 onChange={(e) => {
-                  setFormData({ ...formData, email: e.target.value });
+                  setFormData({ ...formData, isRemember: e.target.checked });
                 }}
               />
-              <input
-                type="password"
-                value={formData.password}
-                autoComplete="off"
-                onKeyPress={onPressEnter}
-                onChange={(e) => {
-                  setFormData({
-                    ...formData,
-                    password: e.target.value,
-                  });
-                }}
-              />
-            </form>
-            <div className="login_question">
-              <label>
-                <input
-                  type="checkbox"
-                  onChange={(e) => {
-                    setFormData({ ...formData, isRemember: e.target.checked });
-                  }}
-                />
-                로그인 상태 유지
-              </label>
+              로그인 상태 유지
+            </label>
 
-              <label className="forget_password_lbl">비밀번호 찾기</label>
-            </div>
-            <div className="btn_box">
-              <button onClick={onClinkLogin}>로그인</button>
-              <button>가입하기</button>
-            </div>
-            <div>
-              <ul className="icon_ul">
-                <li>
-                  <label
-                    onClick={() => {
-                      onClickSocial("");
-                    }}
-                  >
-                    <img src={google} alt="google_icon" />
-                  </label>
-                </li>
-                <li>
-                  <label
-                    onClick={() => {
-                      onClickSocial("");
-                    }}
-                  >
-                    <img src={kakao} alt="kakao_icon" />
-                  </label>
-                </li>
-                <li>
-                  <label
-                    onClick={() => {
-                      onClickSocial("");
-                    }}
-                  >
-                    <img src={naver} alt="naver_icon" />
-                  </label>
-                </li>
-              </ul>
-            </div>
+            <label className="forget_password_lbl">비밀번호 찾기</label>
+          </div>
+          <div className="btn_box">
+            <button onClick={onClinkLogin}>로그인</button>
+            <button>가입하기</button>
+          </div>
+          <div>
+            <ul className="icon_ul">
+              <li>
+                <label
+                  onClick={() => {
+                    onClickSocial("");
+                  }}
+                >
+                  <img src={google} alt="google_icon" />
+                </label>
+              </li>
+              <li>
+                <label
+                  onClick={() => {
+                    onClickSocial("");
+                  }}
+                >
+                  <img src={kakao} alt="kakao_icon" />
+                </label>
+              </li>
+              <li>
+                <label
+                  onClick={() => {
+                    onClickSocial("");
+                  }}
+                >
+                  <img src={naver} alt="naver_icon" />
+                </label>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
-    </article>
+    </div>
   );
 };
 
