25cac7245ebd9d1bfca65553c6c050ed4d28129f
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-10-31 18:08:37 +0900

    Feat :modify Login

--- views/src/components/Main.tsx
+++ views/src/components/Main.tsx
@@ -1,14 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ChatRoom from "./ChatRoom";
 import ChatTab from "./ChatTab";
+import Header from "./Header";
+
+let webSocket: WebSocket;
 
 const Main = (props: any) => {
   const callAPI: Function = props.callAPI;
+  const [isOnReady, setIsOnReady] = useState<boolean>(false);
 
-  const webSocket: WebSocket = props.webSocket;
+  const webSocketInit = () => {
+    webSocket = new WebSocket("ws://localhost:1225");
+
+    if (!isOnReady) {
+      webSocket.onopen = (e) => {
+        console.info("Server Connected");
+        setIsOnReady(true);
+      };
+    }
+    webSocket.onclose = (e) => {
+      console.error("Re Try Server Connecting...");
+      webSocketInit();
+      setIsOnReady(false);
+    };
+    webSocket.onerror = (e) => {
+      console.error(`WebSocket Error : ${e}`);
+      setIsOnReady(false);
+    };
+  };
 
   useEffect(() => {
     !props.isLogined && loginCheck();
+    webSocketInit();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -19,16 +42,31 @@ const Main = (props: any) => {
     });
     window.sessionStorage.setItem("nickname", res.nickname);
     props.setIsLogined(res.isLogined);
+
+    if (!res.isLogined) {
+      props.history.push("/login");
+    }
   };
   return (
-    <div className="main_box">
-      <ChatTab key="chatTab_1" callAPI={callAPI} isLogined={props.isLogined} />
-      <ChatRoom
-        key="chatbox_1"
+    <>
+      <Header
+        setIsLogined={props.setIsLogined}
+        callAPI={callAPI}
         webSocket={webSocket}
-        isOnReady={props.isOnReady}
       />
-    </div>
+      <div className="main_box">
+        <ChatTab
+          key="chatTab_1"
+          callAPI={callAPI}
+          isLogined={props.isLogined}
+        />
+        <ChatRoom
+          key="chatbox_1"
+          webSocket={webSocket}
+          isOnReady={props.isOnReady}
+        />
+      </div>
+    </>
   );
 };
 
